7ada766727e80e4ed6569364fa3a30e8303abe59
Alejandro De Santiago <adesa002@ucr.edu>
2025-09-15 19:40:06 -0400

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -268,7 +268,7 @@ let determineComputedTheme = () => {
   if (themeSetting == "system") {
     const userPref = window.matchMedia;
     if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      return "dark";
+      return "light";
     } else {
       return "light";
     }
