269826c72274550019d68f2140d4e8ea42d4bd2d
alejandrodesantiago <adesa002@ucr.edu>
2025-09-15 23:29:02 +0000

    Deploying to gh-pages from @ alejandrodesantiago/alejandrodesantiago.github.io@78e351f52b005ee4db7b4db99adcb87f720354c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-new-preprint-published-pseudoalteromonas-is-a-novel-symbiont-of-marine-invertebrates-that-exhibits-broad-patterns-of-phylosymbiosis",
+          title: 'New preprint published! Pseudoalteromonas is a novel symbiont of marine invertebrates that exhibits...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
